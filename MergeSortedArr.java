c66db9a9a0a84efaed57f86aed5df74b7f0e3500
Antu77Tyagi <134777004+Antu77Tyagi@users.noreply.github.com>
2023-05-29 18:41:38 +0530

    Update MergeSortedArr.java

--- MergeSortedArr.java
+++ MergeSortedArr.java
@@ -4,24 +4,17 @@ import java.util.Scanner;
 public  class MergeSortedArr {
     public static void main(String[] args) {
         int n=0,m=1;
-        int arr1[]={0};
-        int arr2[]={1};
-        // Scanner sc=new Scanner(System.in);
-        // int arr1[]=new int[n+m];
-        // int arr2[]=new int [m];
-        // System.out.println("enter the first array element");
-        // for(int i=0;i<n;i++){
-        //     arr1[i]=sc.nextInt();
-        // }
-        // System.out.println("enter the second array element");
-        // for(int i=0;i<m;i++){
-        //     arr2[i]=sc.nextInt();
-        // }
-        // System.out.println("before");
-        // for(int i=0;i<arr1.length;i++){
-        //     System.out.print(arr1[i]+" ");
-        // }
-        
+        Scanner sc=new Scanner(System.in);
+        int arr1[]=new int[n+m];
+        int arr2[]=new int [m];
+        System.out.println("enter the first array element");
+        for(int i=0;i<n;i++){
+            arr1[i]=sc.nextInt();
+        }
+        System.out.println("enter the second array element");
+        for(int i=0;i<m;i++){
+            arr2[i]=sc.nextInt();
+        }
         for(int i=0;i<arr2.length;i++){
               if(n<arr1.length){
             arr1[n]=arr2[i];
@@ -29,7 +22,6 @@ public  class MergeSortedArr {
             }
         }
         Arrays.sort(arr1);
-        System.out.println("after");
         System.out.println();
         for(int i=0;i<arr1.length;i++){
             System.out.print(arr1[i]+" ");
