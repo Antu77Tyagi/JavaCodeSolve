216f17103158e7cfa89f0c4ed8a37d3fe3da847a
Antu77Tyagi <134777004+Antu77Tyagi@users.noreply.github.com>
2023-06-02 15:12:08 +0530

    Update RotateAnArrayK.java

--- RotateAnArrayK.java
+++ RotateAnArrayK.java
@@ -17,8 +17,6 @@ public class RotateAnArrayK {
     public static void main(String[] args) {
         int arr[]={1,2,3,4,5,6,7};
         rotate(arr, 3);
-        System.out.println(arr.length);
-        System.out.println(arr.length-1);
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
