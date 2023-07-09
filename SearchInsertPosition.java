84e60cfde2a7ed3df850a2dfc0f6eba0c9f8b09c
Antu77Tyagi <134777004+Antu77Tyagi@users.noreply.github.com>
2023-07-09 14:46:21 +0530

    Update SearchInsertPosition.java

--- SearchInsertPosition.java
+++ SearchInsertPosition.java
@@ -1,3 +1,7 @@
+
+//Leetcode problem no 35;
+//Given a sorted array of distinct integers and a target value, return the index if the target is found. If not,
+  //   return the index where it would be if it were inserted in order.
 public class SearchInsertPosition{
      static int searchInsert(int[] nums, int target) {
        
